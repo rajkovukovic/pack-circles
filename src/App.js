876f1b5782542140fbaebbd5aca50a588fdfd6a7
Rajko Vukovic <rajkovukovic@gmail.com>
2018-10-24 15:25:48 +0200

    first 2d packing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css';
+
+import {
+  generateCircles,
+  packCircles
+} from './packCircles'
+
+const CIRCLE_COUNT = 100
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    const rawCircles = generateCircles(CIRCLE_COUNT)
+    const laneWidth = 200
+    this.state = {
+      ...this.repositionCircles({
+        laneWidth,
+        rawCircles,
+        doSetState: false
+      })
+    }
+  }
+
+  repositionCircles({laneWidth, rawCircles = this.state.rawCircles, doSetState = true}) {
+    const nextState = {
+      laneWidth,
+      rawCircles: rawCircles,
+      ...packCircles({
+        circles: rawCircles,
+        laneWidth,
+      })
+    }
+    if (doSetState) {
+      this.setState(nextState)
+    }
+    return nextState
+  }
+
   render() {
+    const { laneWidth, laneHeight, circles } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <>
+        <div className='range-container'>
+          <InputRange
+            minValue={100}
+            maxValue={600}
+            value={laneWidth}
+            onChange={value => this.repositionCircles({ laneWidth: value })}
+          />
+        </div>
+        <div className='circle-container' style={{width: laneWidth}}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={laneWidth}
+            height={laneHeight}
+            viewBox={`0 0 ${laneWidth} ${laneHeight}`}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            {(circles || []).map(({id, r, x, y}) => (
+              <circle
+                className='svg-circle'
+                key={id}
+                r={r}
+                cx={0}
+                cy={0}
+                style={{
+                  transform: `translate(${x}px, ${y}px)`
+                }}
+              />
+            ))}
+          </svg>
+        </div>
+      </>
     );
   }
 }
---
+++ src/packCircles.js
@@ -0,0 +1,117 @@
+let nextCircleId = 1
+const generateCircles = count => {
+  const minR = 10
+  const maxR = 70
+  const result = []
+  for (let i = 0; i < count; i++) {
+    result.push({
+      id: nextCircleId++,
+      r: Math.round(Math.random() * Math.random() * (maxR - minR) + minR)
+    })
+  }
+  return result
+}
+
+const Slide = {
+  left: -1,
+  right: 1
+}
+
+// const randomOneOrMinusOne = () => (Math.round(Math.random()) - 0.5) * 2
+
+// const calculateLeg = (hypotenuse, leg) => Math.sqrt(hypotenuse ** 2 - leg ** 2)
+
+// const areCirclesOverlapping = (circle1, circle2, minCircleSpacing = 0) =>
+//   (circle1.x - circle2.x) ** 2 + (circle1.y - circle2.y) ** 2 < (circle1.r + circle2.r + minCircleSpacing) ** 2
+
+// const compareCirclesByX = (circle1, circle2) => circle1.x - circle2.x
+
+const offsetCirclesX = (circles, offsetX) => {
+  circles.forEach(circle => circle.x += offsetX)
+  return circles
+}
+
+const packCircles = ({
+  circles: unsortedCircles,
+  laneWidth: laneWidthWithPadding,
+  paddingTop = 0,
+  paddingBottom = 0,
+  paddingLeft = 0,
+  paddingRight = 0,
+  circleSpacing = 10
+}) => {
+  const laneWidth = laneWidthWithPadding - paddingLeft - paddingRight
+  let circles = [...unsortedCircles]
+  // sorting circles by size biggest to smallest
+  circles.sort((circle1, circle2) => circle2.r - circle1.r)
+  // packing circles
+  let firstRow = true
+  let lastSlide = Slide.right // -1 -> shift left, +1 -> shift right, 0 -> unassigned
+  let tailingCircles = [] // circles in "last row" sorted by x coordinate from left to right
+  let currentBottomY = paddingTop
+
+  const centerTailingCirclesHorizontally = (tailingCircles, laneWidth) => {
+    if (tailingCircles.length > 0) {
+      const rightCircle = tailingCircles[tailingCircles.length - 1]
+      offsetCirclesX(tailingCircles, (laneWidth - (rightCircle.x + rightCircle.r)) / 2)
+    }
+  }
+
+  circles = circles
+    .map((circle, currentCircleIndex) => {
+      // first circle in lane
+      if (tailingCircles.length === 0) {
+        circle.x = circle.r
+        circle.y = currentBottomY + circle.r
+        tailingCircles.push(circle)
+      }
+      else {
+        // checking right area
+        const leftCircle = tailingCircles[0]
+        const rightCircle = tailingCircles[tailingCircles.length - 1]
+        // if first row in lane
+        if (rightCircle.x + rightCircle.r + circleSpacing + 2 * circle.r < laneWidth) {
+          if (lastSlide === Slide.right) {
+            // insert current "circle" on the left side of row
+            circle.x = circle.r
+            circle.y = leftCircle.y
+            // move circles right to make room for current "circle"
+            offsetCirclesX(tailingCircles, 2 * circle.r + circleSpacing)
+            tailingCircles.unshift(circle)
+          }
+          else {
+            circle.x = rightCircle.x + rightCircle.r + circleSpacing + circle.r
+            circle.y = rightCircle.y
+            tailingCircles.push(circle)
+          }
+          lastSlide = -lastSlide
+          return circle
+        }
+        else {
+          if (firstRow || currentCircleIndex === circles.length-1) {
+            // center horizontally first row
+            centerTailingCirclesHorizontally(tailingCircles, laneWidth)
+            // firstRow = false
+            currentBottomY = Math.max(...tailingCircles.map(tailingCircle => tailingCircle.y + tailingCircle.r)) + circleSpacing
+            circle.x = circle.r
+            circle.y = currentBottomY + circle.r
+            tailingCircles = [circle]
+          }
+        }
+        // checking areas between circles
+        
+        // checking left area
+      }
+      return circle
+    })
+  centerTailingCirclesHorizontally(tailingCircles, laneWidth)
+  return {
+    circles: offsetCirclesX(circles, paddingLeft),
+    laneHeight: currentBottomY + paddingBottom
+  }
+}
+
+export {
+  generateCircles,
+  packCircles
+}
